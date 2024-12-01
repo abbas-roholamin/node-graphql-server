127e74b026008515abef8dc01084715f204b8e93
Abbas Roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2024-12-01 13:26:28 +0430

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,7 +6,7 @@ export const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false)
     const conn = await mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@hdk.xzd5z.mongodb.net/?retryWrites=true&w=majority&appName=hdk`,
+      `mongodb+srv://abbas_roholamin:iQnvp7QJuf95EbNi@hdk.xzd5z.mongodb.net/?retryWrites=true&w=majority&appName=hdk`,
     )
     console.log("Mongoose Connection Successfully!")
   } catch (err) {
